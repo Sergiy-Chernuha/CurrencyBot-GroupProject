236e822c930b95a821e2ab00c7cafea50ec8aff4
Kahatana22 <120562156+Kahatana22@users.noreply.github.com>
2023-05-21 21:08:24 +0300

    Valya (#13)
    * Create README.md
    
    * Добавить кнопки при нажатии на "Налаштування"
    Добавить выбор сколько знаков после запятой.
    Добавить выбор валюты(одна или несколько)
    
    * Change USD.
    
    * Добавила кнопки сповіщення.
    
    * Добавила кнопки сповіщення.
    
    * Добавила кнопки сповіщення.
    
    * Добавила кнопки сповіщення.
    
    * Проба убрать конфликт.
    
    * Друга спроба прибрати конфлікт
    
    ---------
    
    Co-authored-by: Sergiy-Chernuha <129219908+Sergiy-Chernuha@users.noreply.github.com>
    Co-authored-by: khylyk <85830910+khylyk@users.noreply.github.com>
    Co-authored-by: Sergiy-Chernuha <sergiy.chernuha@gmail.com>

--- src/main/java/ua/goit/telegrambot/MyTelBot.java
+++ src/main/java/ua/goit/telegrambot/MyTelBot.java
@@ -48,26 +48,26 @@ public class MyTelBot extends TelegramLongPollingBot {
                     sendNextMessage(sendMessage);
                 }
                 case ("options") -> sendNextMessage(sendChoiceOptionsMessage(sendMessage));
-                case("bank") -> sendNextMessage(sendChoiceBankMessage(sendMessage));
-                case("decimals") -> sendNextMessage(sendChoiceDecimalsMessage(sendMessage));
-                case("currencies") -> sendNextMessage(sendChoiceCurrenciesMessage(sendMessage));
-                case("USD") -> {
+                case ("bank") -> sendNextMessage(sendChoiceBankMessage(sendMessage));
+                case ("decimals") -> sendNextMessage(sendChoiceDecimalsMessage(sendMessage));
+                case ("currencies") -> sendNextMessage(sendChoiceCurrenciesMessage(sendMessage));
+                case ("USD") -> {
                     options.setChoicesCurrencies(List.of(Currencies.USD));
                     sendNextMessage(sendUpdatedSettingMessage(sendMessage));
                 }
-                case("EUR") -> {
+                case ("EUR") -> {
                     options.setChoicesCurrencies(List.of(Currencies.EUR));
                     sendNextMessage(sendUpdatedSettingMessage(sendMessage));
                 }
-                case("two") -> {
+                case ("two") -> {
                     options.setNumberOfDecimal(2);
                     sendNextMessage(sendUpdatedSettingMessage(sendMessage));
                 }
-                case("three") -> {
+                case ("three") -> {
                     options.setNumberOfDecimal(3);
                     sendNextMessage(sendUpdatedSettingMessage(sendMessage));
                 }
-                case("four") -> {
+                case ("four") -> {
                     options.setNumberOfDecimal(4);
                     sendNextMessage(sendUpdatedSettingMessage(sendMessage));
                 }
@@ -76,6 +76,12 @@ public class MyTelBot extends TelegramLongPollingBot {
                     options.setBank(newBank);
                     sendNextMessage(sendUpdatedSettingMessage(sendMessage));
                 }
+                case ("reminders") -> sendNextMessage(sendChoiceReminderMessage(sendMessage));
+                case ("9"), ("10"), ("11"), ("12"), ("13"), ("14"), ("15"), ("16"), ("17"), ("18") -> {
+                    options.setAlertTime(Integer.parseInt(inputQueryMessage));
+                    sendNextMessage(sendUpdatedSettingMessage(sendMessage));
+                }
+                case ("OffReminder") -> sendNextMessage(sendUpdatedSettingMessage(sendMessage));
                 default -> {
                     sendMessage.setText("Тут може бути ваша реклама): " + update.getCallbackQuery().getData());
                     sendNextMessage(sendMessage);
@@ -142,6 +148,14 @@ public class MyTelBot extends TelegramLongPollingBot {
         return sendMessage;
     }
 
+    private SendMessage sendChoiceReminderMessage(SendMessage sendMessage) {
+        InlineKeyboardMarkup inlineKeyboardMarkup = getChoiceReminderKeyBoard();
+
+        sendMessage.setText("Оберіть час сповіщення:");
+        sendMessage.setReplyMarkup(inlineKeyboardMarkup);
+        return sendMessage;
+    }
+
     private SendMessage sendUpdatedSettingMessage(SendMessage sendMessage) {
         InlineKeyboardMarkup inlineKeyboardMarkup = getDefaultKeyBoard();
 
@@ -198,7 +212,7 @@ public class MyTelBot extends TelegramLongPollingBot {
         return inlineKeyboardMarkup;
     }
 
-    private InlineKeyboardMarkup getChoiceDecimalsKeyBoard(){
+    private InlineKeyboardMarkup getChoiceDecimalsKeyBoard() {
         InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
 
         InlineKeyboardButton inlineKeyboardButton1 = new InlineKeyboardButton("2");
@@ -225,7 +239,7 @@ public class MyTelBot extends TelegramLongPollingBot {
         return inlineKeyboardMarkup;
     }
 
-    private InlineKeyboardMarkup getChoiceOptionsKeyBoard(){
+    private InlineKeyboardMarkup getChoiceOptionsKeyBoard() {
         InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
 
         InlineKeyboardButton inlineKeyboardButton1 = new InlineKeyboardButton("Знаки після коми");
@@ -244,7 +258,7 @@ public class MyTelBot extends TelegramLongPollingBot {
         keyboardButtonsRow3.add(inlineKeyboardButton3);
 
         InlineKeyboardButton inlineKeyboardButton4 = new InlineKeyboardButton("Час сповіщень");
-        inlineKeyboardButton4.setCallbackData("notifications");
+        inlineKeyboardButton4.setCallbackData("reminders");
         List<InlineKeyboardButton> keyboardButtonsRow4 = new ArrayList<>();
         keyboardButtonsRow4.add(inlineKeyboardButton4);
 
@@ -258,7 +272,7 @@ public class MyTelBot extends TelegramLongPollingBot {
         return inlineKeyboardMarkup;
     }
 
-    private InlineKeyboardMarkup getChoiceCurrenciesKeyBoard(){
+    private InlineKeyboardMarkup getChoiceCurrenciesKeyBoard() {
         InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
 
         InlineKeyboardButton inlineKeyboardButton1 = new InlineKeyboardButton("Євро");
@@ -274,6 +288,60 @@ public class MyTelBot extends TelegramLongPollingBot {
         List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
         rowList.add(keyboardButtonsRow1);
         rowList.add(keyboardButtonsRow2);
+
+        inlineKeyboardMarkup.setKeyboard(rowList);
+        return inlineKeyboardMarkup;
+    }
+
+    private InlineKeyboardMarkup getChoiceReminderKeyBoard() {
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+
+        InlineKeyboardButton inlineKeyboardButton1 = new InlineKeyboardButton("9:00");
+        inlineKeyboardButton1.setCallbackData("9");
+        InlineKeyboardButton inlineKeyboardButton2 = new InlineKeyboardButton("10:00");
+        inlineKeyboardButton2.setCallbackData("10");
+        InlineKeyboardButton inlineKeyboardButton3 = new InlineKeyboardButton("11:00");
+        inlineKeyboardButton3.setCallbackData("11");
+        List<InlineKeyboardButton> keyboardButtonsRow3 = new ArrayList<>();
+        keyboardButtonsRow3.add(inlineKeyboardButton1);
+        keyboardButtonsRow3.add(inlineKeyboardButton2);
+        keyboardButtonsRow3.add(inlineKeyboardButton3);
+
+        InlineKeyboardButton inlineKeyboardButton4 = new InlineKeyboardButton("12:00");
+        inlineKeyboardButton4.setCallbackData("12");
+        InlineKeyboardButton inlineKeyboardButton5 = new InlineKeyboardButton("13:00");
+        inlineKeyboardButton5.setCallbackData("13");
+        InlineKeyboardButton inlineKeyboardButton6 = new InlineKeyboardButton("14:00");
+        inlineKeyboardButton6.setCallbackData("14");
+        List<InlineKeyboardButton> keyboardButtonsRow6 = new ArrayList<>();
+        keyboardButtonsRow6.add(inlineKeyboardButton4);
+        keyboardButtonsRow6.add(inlineKeyboardButton5);
+        keyboardButtonsRow6.add(inlineKeyboardButton6);
+
+        InlineKeyboardButton inlineKeyboardButton7 = new InlineKeyboardButton("15:00");
+        inlineKeyboardButton7.setCallbackData("15");
+        InlineKeyboardButton inlineKeyboardButton8 = new InlineKeyboardButton("16:00");
+        inlineKeyboardButton8.setCallbackData("16");
+        InlineKeyboardButton inlineKeyboardButton9 = new InlineKeyboardButton("17:00");
+        inlineKeyboardButton9.setCallbackData("17");
+        List<InlineKeyboardButton> keyboardButtonsRow9 = new ArrayList<>();
+        keyboardButtonsRow9.add(inlineKeyboardButton7);
+        keyboardButtonsRow9.add(inlineKeyboardButton8);
+        keyboardButtonsRow9.add(inlineKeyboardButton9);
+
+        InlineKeyboardButton inlineKeyboardButton10 = new InlineKeyboardButton("18:00");
+        inlineKeyboardButton10.setCallbackData("18");
+        InlineKeyboardButton inlineKeyboardButton = new InlineKeyboardButton("Вимкнути сповіщення");
+        inlineKeyboardButton.setCallbackData("OffReminder");
+        List<InlineKeyboardButton> keyboardButtonsRow = new ArrayList<>();
+        keyboardButtonsRow.add(inlineKeyboardButton10);
+        keyboardButtonsRow.add(inlineKeyboardButton);
+
+        List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
+        rowList.add(keyboardButtonsRow3);
+        rowList.add(keyboardButtonsRow6);
+        rowList.add(keyboardButtonsRow9);
+        rowList.add(keyboardButtonsRow);
 
         inlineKeyboardMarkup.setKeyboard(rowList);
         return inlineKeyboardMarkup;
@@ -315,7 +383,6 @@ public class MyTelBot extends TelegramLongPollingBot {
         return "BlackBot23_bot";
     }
 
-    // добавить имя и токен своего бота, они не подлежат заливке в GitHub
     @Override
     public String getBotToken() {
         return null;
